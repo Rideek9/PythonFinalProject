6b0526147b9e85f4e0f44119be18273d016e93a7
Damian Rabiński <rabinskid@rabdev.pl>
2022-07-09 10:47:16 +0200

    update some file and add new templates and new website - registration

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-"""This is a main file our app. In this file we have all route to all website
- All application is creat in Flask framework, and use MySQL Data Base"""
-
-from flask import Flask , render_template, url_for, request
+from flask import Flask , render_template, url_for, request, redirect
 import database.create_accond as dbcreat
 import database.login_user as dblog
 
 app = Flask(__name__)
 
 
-
 @app.route('/' , methods =['GET', 'POST'])
 def index():
-
     return render_template('index.html')
+
+
+@app.route('/error/<error>', methods = ['GET','POST'])
+def index_error(error):
+    user_error = f"login bądź hasło nieprawidłowe"
+    if error == 'Login-false':
+        return render_template('index.html', error_user = user_error)
+    elif error == 'registration.html':
+        return redirect('/registration.html')
+    else:
+        return render_template('error404.html')
+
 
 @app.route('/login', methods = ['GET', 'POST'])
 def user():
@@ -20,12 +27,16 @@ def user():
         mail = request.form['email']
         password = request.form['password']
         is_exist = dblog.serch_user(mail)
-        error_user = f"Użytkownik bądź hasło są nieprawidłowe"
+        login = 'Login-false'
         if is_exist == None:
-            return render_template('index.html', error_user = error_user )
+            return redirect(url_for('index_error', error = login))
         elif password != is_exist[2]:
-            return render_template('index.html', error_user=error_user)
+            return redirect(url_for('index_error', error = login))
         else:
             db_name = is_exist[3]
             db_index = is_exist[0]
             return render_template('user.html', name = db_name), db_index
+
+@app.route('/registration.html', methods = ['GET','POST'])
+def registration():
+    return render_template('registration.html')
